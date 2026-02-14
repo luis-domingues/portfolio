f6a7cd7aead5f94a3a2a1cc095deeb34e6386c35
luis-domingues <luisdomingues117@gmail.com>
2026-02-14 13:03:30 -0300

    feat(content): setup articles collection and tailwind typography

---
+++ src/content/config.ts
@@ -0,0 +1,8 @@
+import { defineCollection, z } from "astro:content";
+
+
+const articles = defineCollection({
+    schema: z.object({title: z.string(), description: z.string(),date: z.date(),}),
+});
+
+export const collections = {articles}
